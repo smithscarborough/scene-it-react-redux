b6e93a70a9c56a60b4c16c249ccc6c67ba31fb9a
Smith Scarborough <smithscarborough@gmail.com>
2021-04-05 15:52:42 -0500

    adds react & basic redux

---
+++ src/redux/reducers.js
@@ -0,0 +1,12 @@
+const defaultState = {};
+
+export function movieReducer(state = defaultState, action) {
+    switch (action.type) {
+        case ADD_MOVIE:
+            return {}
+        case DELETE_MOVIE:
+            return {}
+        default: 
+            return state;
+    }
+}
